c72983becec5357f98dec582c4e476f2a65ab9ff
WangYeWei <826036140@qq.com>
2021-01-29 15:08:53 +0800

    Update video.vue

--- toutiao2vue/ui/video/video.vue
+++ toutiao2vue/ui/video/video.vue
@@ -1,28 +1,58 @@
 <template>
-		<div
-		:class="['onekit-video',onekitClass]"
-		:style="onekitStyle"
-		:id="onekitId"
-		>
-		<slot></slot>
-		</div>
+  <div
+       :class="['onekit-video',onekitClass]"
+       :style="onekitStyle">
+    <video :src='src'
+           :id="onekitId"
+           controls
+           :poster="poster"
+           ref="vedioDom"></video>
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-import toutiao_behavior from "../../behaviors/toutiao_behavior"
-import onekit_behavior from "../../behaviors/onekit_behavior"
-export default{
+  import toutiao_behavior from "../../behaviors/toutiao_behavior"
+  import onekit_behavior from "../../behaviors/onekit_behavior"
+  export default {
     name: "onekit-video",
-    mixins:[toutiao_behavior, onekit_behavior],
-	props:{
-	
-		
-	},
-	methods:{
-		
-	}
-}
+    mixins: [toutiao_behavior, onekit_behavior],
+    data: () => ({ video: '' }),
+    props: {
+      src: {
+        type: String,
+        required: true,
+      },
+      autoplay: {
+        type: Boolean,
+        default: false
+      },
+      poster: {
+        type: String
+      }
+
+    },
+    mounted() {
+      const videoContext = this.$refs.vedioDom
+      this.video = videoContext
+      this.bindplay()
+    },
+    methods: {
+      bindplay() {
+        this.video.addEventListener('play', () => {
+          this.$emit('bindplay')
+        })
+      }
+    }
+  }
 </script>
 
 <style>
+  .onekit-video {
+    width: 100%;
+  }
+
+  .onekit-video video {
+    width: 100%;
+  }
 </style>
