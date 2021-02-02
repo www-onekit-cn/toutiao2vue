cfbf4867d0dccd6e9e3da3add57d0cffd49679ee
WangYeWei <826036140@qq.com>
2021-02-02 15:08:51 +0800

    .

--- src/page1.vue
+++ src/page1.vue
@@ -1,26 +1,17 @@
 <template>
   <onekit-page id="app">
-    <onekit-canvas canvas-id="my-canvas"></onekit-canvas>
+    <!-- <onekit-canvas canvas-id="my-canvas"></onekit-canvas> -->
+    <onekit-textarea value="hello world"></onekit-textarea>
   </onekit-page>
 
 </template>
 
 <script>
   import OnekitPage from '../toutiao2vue/OnekitPage';
-  import tt from '../toutiao2vue/tt'
+  // import tt from '../toutiao2vue/tt'
   export default OnekitPage({}, {
     onLoad() {
-      const { pixelRatio } = tt.getSystemInfoSync()
-      const canvasCtx = tt.createCanvasContext('my-canvas')
-      canvasCtx.width *= pixelRatio
-      canvasCtx.height *= pixelRatio
-      // 同时需要缩放画布 x，y 方向尺寸
-      canvasCtx.scale(pixelRatio, pixelRatio)
-      canvasCtx.clearRect(0, 0, 300, 225)
 
-      // canvasCtx.fillStyle = this.data.color;
-      canvasCtx.font = "30px sans-serif"
-      canvasCtx.fillText("ByteDance canvas", 0, 30)
     }
   })
 </script>
--- toutiao2vue/ui/textarea/textarea.vue
+++ toutiao2vue/ui/textarea/textarea.vue
@@ -1,28 +1,44 @@
 <template>
-		<div
-		:class="['onekit-textarea',onekitClass]"
-		:style="onekitStyle"
-		:id="onekitId"
-		>
-		<slot></slot>
-		</div>
+  <textarea
+            :class="['onekit-textarea',onekitClass]"
+            :style="onekitStyle"
+            :id="onekitId"
+            v-model="text">
+  </textarea>
 </template>
 
 <script>
-import toutiao_behavior from "../../behaviors/toutiao_behavior"
-import onekit_behavior from "../../behaviors/onekit_behavior"
-export default{
+  import toutiao_behavior from "../../behaviors/toutiao_behavior"
+  import onekit_behavior from "../../behaviors/onekit_behavior"
+  export default {
     name: "onekit-textarea",
-    mixins:[toutiao_behavior, onekit_behavior],
-	props:{
-	
-		
-	},
-	methods:{
-		
-	}
-}
+    data() {
+      return {
+        text: this.value
+      }
+    },
+    mixins: [toutiao_behavior, onekit_behavior],
+    props: {
+      'value': String
+    },
+    created() {
+      // this.text = this.value
+      // console.log(this.text)
+    },
+    methods: {
+      write(event) {
+        console.log('ok', event)
+      }
+    },
+  }
 </script>
 
 <style>
+  .onekit-textarea {
+    width: 100%;
+    resize: none;
+    background: none;
+    outline: none;
+    border: none;
+  }
 </style>
