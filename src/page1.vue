9df7f79ece2faf97ba07e517fc3352e970242ecf
WangYeWei <826036140@qq.com>
2021-02-07 18:09:13 +0800

    .

--- src/page1.vue
+++ src/page1.vue
@@ -1,45 +1,23 @@
 <template>
-  <!-- <onekit-page id="app">
-    <onekit-view>单列选择器</onekit-view>
-    <onekit-picker mode="selector"
-                   :range="array"
-                   @cancel="cancel"
-                   @Change="change"
-                   :disabled="false">
-      <onekit-view>当前选择： {{array[index]}}</onekit-view>
-    </onekit-picker>
-
-    <onekit-view>地区</onekit-view>
-    <onekit-picker mode="region" @Change="regionchange">
-      <onekit-view>当前选择地区：<br>
-        省: {{region[0]}} <br>
-        市：{{region[1]}} <br>
-        区： {{region[2]}}</onekit-view>
-    </onekit-picker>
-
-  </onekit-page> -->
   <onekit-page id='app'>
-    <onekit-view onekit-class="section__title">日期选择器</onekit-view>
-    <onekit-picker mode="date" :value="date" start="1999-09-01" end="2023-09-01" @Change="bindDateChange" fields="year">
-      <onekit-view onekit-class="picker"> 当前选择: {{date}} </onekit-view>
-    </onekit-picker>
-
+    <onekit-input value="hello motherfucker what's my name"
+                  type="dight"
+                  :password="true"
+                  :focus="focus" />
   </onekit-page>
 </template>
 
 <script>
   import OnekitPage from '../toutiao2vue/OnekitPage';
-  // import tt from '../toutiao2vue/tt'
   export default OnekitPage({}, {
     data: {
-      date: "2016-09-01",
+      focus: false
     },
-    bindDateChange: function (e) {
-      console.log("picker发送选择改变，携带值为", e)
-      this.setData({
-        date: e.detail.value
-      })
-    },
+    onLoad() {
+      setTimeout(() => {
+        this.focus = true
+      }, 3000)
+    }
   })
 </script>
 
--- toutiao2vue/ui/input/input.vue
+++ toutiao2vue/ui/input/input.vue
@@ -1,8 +1,21 @@
 <template>
-  <input
-         :class="['onekit-input',onekitClass]"
-         :style="onekitStyle"
-         :id="onekitId" />
+  <div>
+    <input
+           :class="['onekit-input',onekitClass]"
+           :style="onekitStyle"
+           :id="onekitId"
+           :value="value"
+           :pattern="newType"
+           :type="password ? 'password' : '' "
+           :placeholder="placeholder"
+           :disabled="disabled ? 'disabled' : false "
+           :maxlength="maxlength"
+           :autofocus="focusNew? 'autofocus' : '' "
+           @focus="_focus"
+           @blur="_blur"
+           @keydown="_confirm" />
+  </div>
+
 </template>
 
 <script>
@@ -11,15 +24,74 @@
   export default {
     name: "onekit-input",
     mixins: [toutiao_behavior, onekit_behavior],
+    data() {
+      return {
+        focusNew: 'false'
+      }
+    },
     props: {
-
-
+      'value': String,
+      'type': String,
+      'password': Boolean,
+      'placeholder': String,
+      'placeholder-style': String,
+      'disabled': Boolean,
+      'maxlength': {
+        type: Number,
+        default: 140
+      },
+      'focus': Boolean,
+      'cursor-spacing': Number,
+    },
+    computed: {
+      newType() {
+        let newType
+        switch (this.type) {
+        case 'text':
+          newType = 'text'
+          break
+        case 'number':
+          newType = '[0-9]*'
+          break
+        case 'digit':
+          newType = 'number'
+          break
+        default:
+        }
+        return newType
+      }
+    },
+    created() {},
+    watch: {
+      focus: {
+        deep: true,
+        handler(v) {
+          if (v) this.focusNew = 'autofocus'
+        },
+        immediate: true
+      }
     },
     methods: {
-
+      _focus() {
+        this.$emit('Focus')
+      },
+      _blur() {
+        this.$emit('Blur')
+      },
+      _confirm(e) {
+        if (e.key === 'Enter') {
+          this.$emit('Confirm')
+        }
+      }
     }
   }
 </script>
 
 <style>
+  .onekit-input {
+    width: 100%;
+    background: none;
+    outline: none;
+    border: none;
+  }
 </style>
