e41ef91bd1474ed2cc476a28cbe0281d6516e942
WangYeWei <826036140@qq.com>
2021-02-19 18:02:55 +0800

    .

--- toutiao2vue/ui/web-view/web-view.vue
+++ toutiao2vue/ui/web-view/web-view.vue
@@ -1,27 +1,26 @@
 <template>
-		<div
-		:class="['onekit-web-view',onekitClass]"
-		:style="onekitStyle"
-		:id="onekitId"
-		>
-		<slot></slot>
-		</div>
+  <div
+       :class="['onekit-web-view',onekitClass]"
+       :style="onekitStyle"
+       :id="onekitId">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-import toutiao_behavior from "../../behaviors/toutiao_behavior"
-import onekit_behavior from "../../behaviors/onekit_behavior"
-export default{
+  import toutiao_behavior from "../../behaviors/toutiao_behavior"
+  import onekit_behavior from "../../behaviors/onekit_behavior"
+  export default {
     name: "onekit-web-view",
-    mixins:[toutiao_behavior, onekit_behavior],
-	props:{
-	
-		
-	},
-	methods:{
-		
-	}
-}
+    mixins: [toutiao_behavior, onekit_behavior],
+    props: {
+
+
+    },
+    methods: {
+
+    }
+  }
 </script>
 
 <style>
